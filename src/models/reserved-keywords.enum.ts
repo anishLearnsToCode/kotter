08af2efb44c7a6ea5426ee30ec415e96253e7963
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-16 23:19:17 +0200

    Adds a reserved keywords enumerator

---
+++ src/models/reserved-keywords.enum.ts
@@ -0,0 +1,30 @@
+export enum ReservedKeywords {
+  ELSE = 'else',
+  IF = 'if',
+  ELSE_IF = 'else if',
+
+  SWITCH = 'switch',
+  CASE = 'case',
+  DEFAULT = 'default',
+
+  FINALLY = 'finally',
+  TRY = 'try',
+  CATCH = 'catch',
+  THROW = 'throw',
+
+  IMPORT = 'import',
+  NEW = 'new',
+  CLASS = 'class',
+  EXTENDS = 'extends',
+
+  LET = 'let',
+  VAR = 'var',
+  CONST = 'const',
+
+  FOR = 'for',
+  WHILE = 'while',
+  DO_WHILE = 'do',
+
+  FUNCTION = 'function',
+  RETURN = 'return'
+}
